e65b31ad368e7b93efd074f95a483b043b739962
Masato Tokuse <masadinti@gmail.com>
2020-02-24 11:39:36 +0900

    fix TestConnnectGorm

--- backend/webpush/dbaccess/subscription_test.go
+++ backend/webpush/dbaccess/subscription_test.go
@@ -6,7 +6,14 @@ import (
 
 // 疎通確認
 func Test_ConnectGorm_1(t *testing.T) {
-	db := ConnectGorm()
+	conargs := &ConnectArgs{
+		Address:  "localhost",
+		Port:     "33306",
+		DBName:   "webpush",
+		User:     "webpush",
+		Password: "webpush"}
+
+	db := ConnectGorm(conargs)
 	defer db.Close()
 	db.Set("gorm:table_options", "ENGINE = InnoDB").AutoMigrate(&Subscription{})
 }
